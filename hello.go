993a7ac0a8a413580aeed7dcaf4f7815cd4fa93d
takumi <tkm0703@icloud.com>
2022-06-11 21:00:14 +0900

    乱数生成

--- hello.go
+++ hello.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
-
-import "rsc.io/quote"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println(quote.Go())
+	rand.Seed(time.Now().UnixNano()) // Seedを設定しないと、乱数が生成されない
+	fmt.Println("my favorite number is", rand.Intn(10))
 }
