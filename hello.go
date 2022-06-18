3ce4c7f52ecae7a3f2f5975181f74d6ee3ee34ad
takumi <tkm0703@icloud.com>
2022-06-18 19:38:23 +0900

    変数について

--- hello.go
+++ hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
-var i, j int = 1, 2
+var k = 3
+
+// k := 3 NG
 
 func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, j, c, python, java)
+	var i, j int = 1, 2
+	// k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
 }
